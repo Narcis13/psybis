ca4bdad2a956898b5238d08b0007df2aa3610369
Narcis13 <Narcis75@gmail.com>
2023-05-11 00:51:09 +0300

    mai un chatgpt

--- client/src/game/scenes/PlayScene.ts
+++ client/src/game/scenes/PlayScene.ts
@@ -6,7 +6,7 @@ export default class PlayScene extends Scene {
   }
 
   create () {
-    this.add.image(400, 300, 'sky')
+    /*this.add.image(400, 300, 'sky')
 
     const bomb = this.physics.add.image(400, 200, 'bomb')
     bomb.setCollideWorldBounds(true)
@@ -17,7 +17,43 @@ export default class PlayScene extends Scene {
     this.sound.add('thud')
     this.physics.world.on('worldbounds', () => {
       this.sound.play('thud', { volume: 0.75 })
-    })
+    })*/
+
+    var countdownDuration=10;
+    var countdownText = this.add.text(400, 300, ''+countdownDuration, { font: "144px Arial", color: "#ffffff", align: "center" });
+    countdownText.setOrigin(0.5, 0.5);
+    countdownText.setScale(1.5, 1.5);
+    var that=this
+    var timer = this.time.addEvent({
+        delay: 1000,
+        callback: function() {
+            countdownDuration--;
+          //  console.log(countdownDuration)
+          if (countdownDuration > 0)   countdownText.setText(''+countdownDuration);
+
+            var prevCountdownText = countdownText;
+
+            if (countdownDuration >= 0) {
+                countdownText = that.add.text(400, 300, ''+countdownDuration, { font: "144px Arial", color: "#ffffff", align: "center" });
+                countdownText.setOrigin(0.5, 0.5);
+                countdownText.setScale(1.5, 1.5);
+                countdownText.setAlpha(1);
+
+                that.tweens.add({
+                    targets: prevCountdownText,
+                    alpha: 0,
+                    scaleX: 0.5,
+                    scaleY: 0.5,
+                    duration: 500,
+                    onComplete: function() {
+                        prevCountdownText.destroy();
+                    }
+                });
+            }
+        },
+        callbackScope: that,
+        repeat: countdownDuration
+    });
   }
 
   update () {
