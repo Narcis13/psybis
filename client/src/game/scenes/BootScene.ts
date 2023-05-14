e1499e31ed83f3f4998fab4a8e48249c9143f3c4
Narcis13 <Narcis75@gmail.com>
2023-05-14 18:57:11 +0300

    deocamdata

--- client/src/game/scenes/BootScene.ts
+++ client/src/game/scenes/BootScene.ts
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser'
+import eventsCenter from './EventsCenter'
 import sky from '@/game/assets/sky.png'
 import bomb from '@/game/assets/bomb.png'
 import butonstart from '@/game/assets/butonstart.png'
@@ -6,10 +7,13 @@ import thudMp3 from '@/game/assets/thud.mp3'
 import thudOgg from '@/game/assets/thud.ogg'
 
 export default class BootScene extends Scene {
+  
+  testdata={}
+  
   constructor () {
     super({ key: 'BootScene' })
   }
-
+  
   preload () {
     this.load.image('sky', sky)
     this.load.image('buton_start',butonstart)
@@ -18,10 +22,27 @@ export default class BootScene extends Scene {
   }
 
   create () {
+   this.testdata.items=[
+      {
+        item:'Care este culoarea ta preferata?',
+        type:'static',
+        subitems:['1. Albastru','2. Rosu','3. Verde','4. Negru']
+      },
+      {
+        item:'Unde ati prefera sa va petreceti concediul?',
+        type:'static',
+        subitems:['1. Intr-o tara nordica, rece!','2. Aproape de casa!','3. Intr-o zona calda, la mare!','4. La munte pentru a practica sport extrem!']
+      }
+
+   ]
+ 
+
    const btn= this.add.image(800,1000,'buton_start')
    btn.setInteractive()
    btn.on('pointerdown',()=>{
-    this.scene.start('PlayScene')
+    eventsCenter.emit('date-sosite',this.testdata.items) 
+    this.scene.run('PlayScene')
+    btn.setVisible(false)
    })
    // this.scene.start('PlayScene')
    
--- client/src/game/scenes/PlayScene.ts
+++ client/src/game/scenes/PlayScene.ts
@@ -1,116 +1,69 @@
 import { Scene } from 'phaser'
-
+import eventsCenter from './EventsCenter'
 export default class PlayScene extends Scene {
-  index 
+  index =0
+  items={}
   questionText =null
-
+  answerTexts = []
   constructor () {
     super({ key: 'PlayScene' })
     this.index=0
   }
 
-init(){
-  console.log('init')
-this.cursors = this.input.keyboard.createCursorKeys()
-}
-
-   afiseazaIntrebare(){
-    //console.log('afisez intrebare')
-    var question = "What is the capital of France?  ";
-    var answerOptions = ["Paris", "Madrid", "Rome", "Berlin"];
-    var correctAnswerIndex = 0; // Index of the correct answer in answerOptions
+  init(){
+    console.log('init')
     
-    this.questionText = this.add.text(800, 200+Math.random()*20, question, { font: "72px Arial", color: "#ffffff", align: "center" ,wordWrap:{width:1600}});
-    this.questionText.setOrigin(0.5, 0.5);
+   this.cursors = this.input.keyboard.createCursorKeys()
+  }
 
-    // Create the answer option texts
-    var answerTexts = [];
-    for (var i = 0; i < answerOptions.length; i++) {
-        var answerText = this.add.text(400, 300 + (i * 50), answerOptions[i], { font: "24px Arial", color: "#ffffff", align: "center" });
-        answerText.setOrigin(0.5, 0.5);
-        answerTexts.push(answerText);
-        var that=this
-        // Set up a click event for each answer option
-        answerText.setInteractive();
-        this.input.on('gameobjectdown', function (pointer, gameObject) {
-            if (answerTexts.indexOf(gameObject) === correctAnswerIndex) {
-                // The clicked answer is correct, so remove the question and answer texts
-                that.questionText.setVisible(false)
-               // questionText.destroy();
-                for (var i = 0; i < answerTexts.length; i++) {
-                    answerTexts[i].destroy();
-                }
-                that.index++
-              //  that.afiseazaIntrebare();
-            }
-        });
-      }
+   afiseazaIntrebare(idx:number){
+    console.log('afisez intrebare',idx)
+    var question = this.items[idx].item;
+    var answerOptions = this.items[idx].subitems;
+    this.questionText.setText(question)
+   // var correctAnswerIndex = 0; // Index of the correct answer in answerOptions
+    
+
    }
 
   create () {
-    /*this.add.image(400, 300, 'sky')
-
-    const bomb = this.physics.add.image(400, 200, 'bomb')
-    bomb.setCollideWorldBounds(true)
-    bomb.body.onWorldBounds = true // enable worldbounds collision event
-    bomb.setBounce(1)
-    bomb.setVelocity(200, 20)
-
-    this.sound.add('thud')
-    this.physics.world.on('worldbounds', () => {
-      this.sound.play('thud', { volume: 0.75 })
-    })*/
-
-    var countdownDuration=10;
-    var countdownText = this.add.text(400, 300, ''+countdownDuration, { font: "144px Arial", color: "#ffffff", align: "center" });
-    countdownText.setOrigin(0.5, 0.5);
-    countdownText.setScale(1.5, 1.5);
+    //console.log('PlayScene a fost creata')
     var that=this
-    var timer = this.time.addEvent({
-        delay: 1000,
-        callback: function() {
-            countdownDuration--;
-          //  console.log(countdownDuration)
-          if (countdownDuration > 0)  {
-            countdownText.setText(''+countdownDuration)
-          }  
-          else
-          countdownText.destroy()
-          if(countdownDuration==0) {
-            countdownText.destroy()
-            this.afiseazaIntrebare()
-          };
+    eventsCenter.on('date-sosite',(items)=>{
+      console.log('Eveniment date sosiste receptionat',items)
+      that.items=items
+   
+    this.questionText = this.add.text(800, 200, '', { font: "72px Arial", color: "#ffffff", align: "center" ,wordWrap:{width:1600}});
+    this.questionText.setOrigin(0.5, 0.5);
 
-            var prevCountdownText = countdownText;
+    // Create the answer option texts
+   //console.log(this.items[this.index])
+    for (var i = 0; i < items[0].subitems.length; i++) {
+        var answerText = this.add.text(400, 300 + (i * 50), '', { font: "24px Arial", color: "#ffffff", align: "center" });
+        answerText.setOrigin(0.5, 0.5);
+        this.answerTexts.push(answerText);
+        
+        // Set up a click event for each answer option
+        answerText.setInteractive();
+ 
+      }
+      this.afiseazaIntrebare(this.index)
+    })
 
-            if (countdownDuration >= 0) {
-                countdownText = that.add.text(400, 300, ''+countdownDuration, { font: "144px Arial", color: "#ffffff", align: "center" });
-                countdownText.setOrigin(0.5, 0.5);
-                countdownText.setScale(1.5, 1.5);
-                countdownText.setAlpha(1);
-
-                that.tweens.add({
-                    targets: prevCountdownText,
-                    alpha: 0,
-                    scaleX: 0.5,
-                    scaleY: 0.5,
-                    duration: 500,
-                    onComplete: function() {
-                        prevCountdownText.destroy();
-                    }
-                });
-            }
-        },
-        callbackScope: that,
-        repeat: countdownDuration
+      this.input.on('gameobjectdown', function ( go) {
+        console.log('gameObject clicked',go)
     });
+
+   
+
+
   }
 
   update () {
 
     if (this.cursors.down.isDown){
       console.log('cursor down')
-      this.questionText.setVisible(true)
+     
     }
   }
 }
