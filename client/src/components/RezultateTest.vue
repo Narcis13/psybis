d96b357517f991ca717be109c7a16b6acc387fc6
Narcis13 <Narcis75@gmail.com>
2023-06-08 20:27:10 +0300

    asa da

--- client/src/components/RezultateTest.vue
+++ client/src/components/RezultateTest.vue
@@ -6,58 +6,10 @@ const suntEvenimente = computed(()=>{
     return props.evenimente.lista&&props.evenimente.lista.length>0
 })
 
-let state = reactive({
-   rezultate:{
-    durataStartTest:0
-   } 
-})
-let states={
-    grafic_cadran_sus:{
-                     options:{
-                            chart: {
-                                    id: 'grafic_cadran_sus'
-                                 },
-                                xaxis: {
-                                    categories: []
-                                }
-                         },
-                         series:[{
-                                 name: 'timp_reactie_cadran_sus',
-                                 data: []
-                           }]
-                     }
-}
 
 onUpdated(()=>{
     
-    let reactii_cadran_sus=0
-    let reactii_inutile_cadran_sus=0
-    let timp_reactie_cadran_sus=0
-    let timpi_reactie_cadran_sus=[]
-    let indecsi_reactie_cadran_sus=[]
-
-    if(props.evenimente.lista){
-        props.evenimente.lista.map(e=>{
-            if(e.reactie=='corect'&&e.element=='cadran_sus') {
-                reactii_cadran_sus+=1
-                timp_reactie_cadran_sus+=(e.momentReactie-e.startStimul)
-                timpi_reactie_cadran_sus.push(Math.floor(e.momentReactie-e.startStimul))
-                indecsi_reactie_cadran_sus.push(timpi_reactie_cadran_sus.length)
-
-            }
-
-            if(e.reactie=='inutil'&&e.element=='cadran_sus') reactii_inutile_cadran_sus+=1
-        })
-        state.rezultate.durataStartTest=Math.floor((props.evenimente.startReprizaI-props.evenimente.startAntrenament)/1000)
-        state.rezultate.reactii_cadran_sus=reactii_cadran_sus
-        state.rezultate.exactitate_reactii_cadran_sus = (reactii_cadran_sus/(props.evenimente.totalStimuliReprizaI[0]+props.evenimente.totalStimuliReprizaII[0])*100).toFixed(2)
-        state.rezultate.reactii_inutile_cadran_sus = reactii_inutile_cadran_sus
-        state.rezultate.timp_mediu_reactie_cadran_sus = Math.floor(timp_reactie_cadran_sus/reactii_cadran_sus)
-
-        states.grafic_cadran_sus.options.xaxis.categories=indecsi_reactie_cadran_sus;
-        states.grafic_cadran_sus.series[0].data=timpi_reactie_cadran_sus;
-    }
-    console.log('updated',state.rezultate)
+    
 })
 
 </script>
@@ -69,22 +21,11 @@ onUpdated(()=>{
             {{ state.rezultate.durataStartTest }} s.
 
          </div> -->
-    <!-- <div>
-        Reactii cadran sus: {{ state.rezultate.reactii_cadran_sus }}
-    </div>
-    <div>
-        Exactitate reactii cadran sus: {{ state.rezultate.exactitate_reactii_cadran_sus }} %
-    </div>
-    <div>
-        Reactii inutile cadran sus: {{ state.rezultate.reactii_inutile_cadran_sus }} 
-    </div>
-    <div>
-       Timp mediu reactie cadran sus: {{ state.rezultate.timp_mediu_reactie_cadran_sus }} ms. 
-    </div> -->
-    <!-- <div>
-        <apexchart width="400" type="line" :options="states.grafic_cadran_sus.options" :series="states.grafic_cadran_sus.series"></apexchart>
-    </div> -->
-   <widget-rezultate element="cadran_sus" :rezultate="state.rezultate" :dategrafic="states" :evenimente="props.evenimente"></widget-rezultate>
+
+   <widget-rezultate element="cadran_sus"  :evenimente="props.evenimente" />
+   <widget-rezultate element="cadran_stanga"  :evenimente="props.evenimente" />
+   <widget-rezultate element="cadran_dreapta"  :evenimente="props.evenimente" />
+   <widget-rezultate element="bara"  :evenimente="props.evenimente" />
     </div>
 
 </template>
--- client/src/components/WidgetRezultate.vue
+++ client/src/components/WidgetRezultate.vue
@@ -1,44 +1,89 @@
 <script setup lang="ts">
-import {ref,onUpdated} from 'vue'
-const props=defineProps(['element','rezultate','dategrafic','evenimente'])
+import {reactive,onUpdated} from 'vue'
+const props=defineProps(['element','evenimente'])
 
 let element=props.element
-let rezultate=props.rezultate
-console.log('Widget...')
-let state = {
-               
-   }
-onUpdated(()=>{
-    console.log('widget updated',props.evenimente,props.rezultate)
-    state= {
-                exactitate_reactii:(props.rezultate['exactitate_reactii_'+element]),
-            reactii:(props.rezultate['reactii_'+element]),
-            reactii_inutile:(props.rezultate['reactii_inutile_'+element]),
-             timp_mediu_reactie:(props.rezultate['timp_mediu_reactie_'+element])
-       }
-
+let idx=0
+if(element=='cadran_stanga') idx=1;
+if(element=='cadran_dreapta') idx=2;
+if(element=='bara') idx=3;
+let state = reactive({
+   rezultate:{
+    durataStartTest:0
+   } 
 })
+let states={
+    grafic_cadran_sus:{
+                     options:{
+                            chart: {
+                                    id: 'grafic cadran_sus'
+                                 },
+                                xaxis: {
+                                    categories: []
+                                }
+                         },
+                         series:[{
+                                 name: 'timp_reactie cadran_sus',
+                                 data: []
+                           }]
+                     }
+}
+onUpdated(()=>{
 
-let optiunigrafic=props.dategrafic['grafic_'+element].options
-let seriegrafic=props.dategrafic['grafic_'+element].series
+
+    let reactii=0
+    let reactii_inutile=0
+    let timp_reactie=0
+    let timpi_reactie=[]
+    let indecsi_reactie=[]
+
+    if(props.evenimente.lista){
+        props.evenimente.lista.map(e=>{
+            if(e.reactie=='corect'&&e.element==element) {
+                reactii+=1
+                timp_reactie+=(e.momentReactie-e.startStimul)
+                timpi_reactie.push(Math.floor(e.momentReactie-e.startStimul))
+                indecsi_reactie.push(timpi_reactie.length)
+
+            }
+
+            if(e.reactie=='inutil'&&e.element==element) reactii_inutile+=1
+        })
+        state.rezultate.durataStartTest=Math.floor((props.evenimente.startReprizaI-props.evenimente.startAntrenament)/1000)
+        state.rezultate.reactii=reactii
+        state.rezultate.exactitate_reactii = (reactii/(props.evenimente.totalStimuliReprizaI[idx]+props.evenimente.totalStimuliReprizaII[idx])*100).toFixed(2)
+        state.rezultate.reactii_inutile = reactii_inutile
+        state.rezultate.timp_mediu_reactie = Math.floor(timp_reactie/reactii)
+        state.rezultate.denumire_element = element.split('_').join(' ')
+        states.grafic_cadran_sus.options.chart.id='grafic_'+element;
+        states.grafic_cadran_sus.options.xaxis.categories=indecsi_reactie;
+        states.grafic_cadran_sus.series[0].data=timpi_reactie;
+        states.grafic_cadran_sus.series[0].name='timp_reactie_'+element;
+    }
+})
+//let exactitate_reactii=(props.rezultate['exactitate_reactii_'+element])
+//let optiunigrafic=props.dategrafic['grafic_'+element].options
+//let seriegrafic=props.dategrafic['grafic_'+element].series
 </script>
 <template>
-        <div>
+        <div class="row" style="width:800px">
+            <div class="col-6 q-mt-sm ">
+                <div class="text-subtitle1">
+                  Reactii {{ state.rezultate.denumire_element }}: <span class="text-indigo-9 text-h6 text-weight-bold">{{state.rezultate.reactii }}</span>
+                </div>
+                <div class="text-subtitle1">
+                    Exactitate reactii {{ state.rezultate.denumire_element }}: <span class="text-indigo-9 text-h6 text-weight-bold">{{ state.rezultate.exactitate_reactii }} %</span>
+                </div>
+                <div class="text-subtitle1">
+                    Reactii inutile {{ state.rezultate.denumire_element }}: <span class="text-indigo-9 text-h6 text-weight-bold">{{ state.rezultate.reactii_inutile }} </span>
+                </div>
+                <div class="text-subtitle1">
+                     Timp mediu reactie {{ state.rezultate.denumire_element }}: <span class="text-indigo-9 text-h6 text-weight-bold">{{ state.rezultate.timp_mediu_reactie }} ms. </span>
+                </div>
+            </div>
 
-            <div>
-                 Reactii cadran sus: {{state.reactii }}
-             </div>
-            <div>
-                Exactitate reactii cadran sus: {{ state.exactitate_reactii }} %
-            </div>
-            <div>
-                Reactii inutile cadran sus: {{ state.reactii_inutile }} 
-            </div>
-            <div>
-            Timp mediu reactie cadran sus: {{ state.timp_mediu_reactie }} ms. 
-            </div>
-            <div>
-                <apexchart width="400" type="line" :options="optiunigrafic" :series="seriegrafic"></apexchart>
+            <div class="col-6">
+                <apexchart width="400" type="line" :options="states.grafic_cadran_sus.options" :series="states.grafic_cadran_sus.series"></apexchart>
             </div>
         </div>
 </template>
